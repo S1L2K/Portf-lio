b25cf322b720e121c3f0c6c8c54cf83131ea5d6f
devryu <150612071+S1L2K@users.noreply.github.com>
2024-10-06 11:50:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Menu de navegação suave
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
